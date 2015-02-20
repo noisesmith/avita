d08f08312e99c210972e56705cb7986f7a7cc128
Justin Smith <noisesmith@gmail.com>
2015-02-20 09:28:22 -0800

    enlive rendering

--- src/avita/handler.clj
+++ src/avita/handler.clj
@@ -1,10 +1,11 @@
 (ns avita.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [net.cgrand.enlive-html :as html]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (html/emit* (html/html [:h1 "hello"])))
   (GET "/submit" [] "Foo")
   (route/not-found "Not Found"))
 
